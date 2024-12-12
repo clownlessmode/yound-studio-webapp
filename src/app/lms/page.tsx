8f2709b13cc29dae9a4d39f84489342b4d55a058
Kovalenko Rodion <167234046+clownlessmode@users.noreply.github.com>
2024-12-12 19:29:17 +0700

    fix

--- src/app/lms/page.tsx
+++ src/app/lms/page.tsx
@@ -22,7 +22,7 @@ const Page = () => {
 
   return (
     <>
-      <main className="pb-[40px] flex-1 overflow-y-auto h-full flex-grow px-[15px] w-full flex flex-col bg-[#8ECFFD]">
+      <main className="pb-[40px] flex-1 overflow-y-auto h-full flex-grow px-[15px] w-full flex flex-col bg-[#C5F3AB]">
         <section className="flex flex-row gap-[7.4px] items-center justify-between mt-[40px] ">
           <div
             className="w-6 h-6 bg-white/30 opacity-0 rounded-full my-2 top-1 right-3 backdrop-blur-lg flex items-center justify-center"
@@ -121,15 +121,10 @@ const Page = () => {
         </section>
 
         <section className="bg-white rounded-[30px]  w-full p-[25px] font-semibold text-[24px] text-[#202022]">
-          <h1>Приложения и визитки</h1>
+          <h1>Искуственный интеллект</h1>
           <div className="h-[1px] w-full bg-[#d9d9dc] my-[15px] "></div>
           <p className="font-medium text-[16px] text-[#202022] leading-[19px]">
-            Здесь можно создать полноценное приложение. Например,
-            онлайн-магазин, игру или сервис для клиентов. Все, что угодно.
-            <br />
-            Представьте, что вы в App Store или Google Play, вы устанавливаете
-            любое приложение и пользуетесь им. Теперь это не нужно, все доступно
-            в внутри Телеграма.
+            Текст не предоставлен
           </p>
         </section>
       </main>
